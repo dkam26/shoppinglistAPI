e66964f935e6fe71ce1c38ed87975d06915024f8
kamara deo <deo.kamara@andela.com>
2017-12-20 11:51:18 +0300

    travis automation

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
 import psycopg2
-db_url='postgresql://deo:masiko26@localhost/shoppinglist'
+db_url='postgresql://postgres:masiko26@localhost/shoppinglist'
 app=Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SECRET_KEY']='Random key'

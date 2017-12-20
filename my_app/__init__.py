ac0b55e26678e603e4321e973b3dcc88ba9a395b
dkamara <gkam1989@gmail.com>
2017-12-20 11:16:08 +0300

    Update __init__.py

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -2,14 +2,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
 import psycopg2
+import os
 db_url='postgresql://deo:masiko26@localhost/shoppinglist'
 app=Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SECRET_KEY']='Random key'
 if os.environ.get('DATABASE_URL'):
-    app.config['SQLALCHEMY_DATABASE_URI']=os.environ['DATABASE_URI']
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URI']
 else:
-    app.config['SQLALCHEMY_DATABASE_URI']=db_url
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 db=SQLAlchemy(app)
 api = Api(app)
 from my_app import views
